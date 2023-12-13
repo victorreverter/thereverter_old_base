79a4b0f5e1f0ebcc31fbcd4575c54d739848fa76
victorreverter <victormanuel.cuba@gmail.com>
2023-12-13 05:57:35 +0100

    Initial commit 2

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Hello World 2!</h1>
+    </div>
+  )
+}
+
+export default App
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello World!</h1>
+//     </div>
+//   );
+// }
+
+// export default App;

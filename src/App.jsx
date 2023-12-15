a64d89ec2495c350fc6f219db42ca077da434915
Víctor Manuel Herrera <vmherrera@ip-192-168-10-60.eu-west-1.compute.internal>
2023-12-15 14:35:02 +0100

    Reformulate Blog Test try 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.js
 // import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import PostDetailPage from './components/PostDetailPage';
 
